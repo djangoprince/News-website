1008c98a22306c25b968629dcf3d7032f64aa0ab
djangoprince <57494042+djangoprince@users.noreply.github.com>
2021-10-19 16:20:48 +0000

    Initial commit

---
+++ HBV501G-Fhruturinn/src/main/java/stu/hi/HBV501GFhruturinn/Hbv501GFhruturinnApplication.java
@@ -0,0 +1,13 @@
+package stu.hi.HBV501GFhruturinn;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Hbv501GFhruturinnApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Hbv501GFhruturinnApplication.class, args);
+	}
+
+}
---
+++ HBV501G-Fhruturinn/src/test/java/stu/hi/HBV501GFhruturinn/Hbv501GFhruturinnApplicationTests.java
@@ -0,0 +1,13 @@
+package stu.hi.HBV501GFhruturinn;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Hbv501GFhruturinnApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
