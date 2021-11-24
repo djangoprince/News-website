e12aa4a48e2f05539b800ff425a0085a9d2f5e9e
brynjarj13 <brynjar1993@gmail.com>
2021-11-24 18:34:54 +0000

    Update UserController.java

--- HBV501G-Fhruturinn/src/main/java/stu/hi/HBV501GFhruturinn/Controllers/UserController.java
+++ HBV501G-Fhruturinn/src/main/java/stu/hi/HBV501GFhruturinn/Controllers/UserController.java
@@ -1,15 +1,15 @@
-package HBV501G_Fhruturinn.src.main.java.stu.hi.HBV501GFhruturinn.Controllers;
+package stu.hi.HBV501GFhruturinn.Controllers;
 
-import org.springframework.stereotype.Controller;
-import stu.hi.HBV501GFhruturinn.Services.UserService.java;
-import org.springframework.security.web.authentication.logout.SecurityContextLogoutHandler;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import stu.hi.HBV501GFhruturinn.Persistence.Entities.User;
+import stu.hi.HBV501GFhruturinn.Services.UserService;
+
 import javax.servlet.http.HttpSession;
-import stu.hi.HBV501GFhruturinn.Persistence.Entities.User.java;
 
 //This controller will get and send the data of users, like then they log in, an accound gets created or deleted
 
@@ -19,7 +19,7 @@ public class UserController {
     private User user;
     private boolean error;
 
-    //@Autowired
+    @Autowired
     public UserController(UserService userService) {
         this.userService = userService;
     }
@@ -31,38 +31,37 @@ public class UserController {
     // loggedin (GET) if not logged in -> redirect to homepage, otherwise it will display my username and possibly recipes?
 
     @RequestMapping(value = "/signup", method = RequestMethod.GET)
-    public String signupGET(User user){
+    public String signupGET(User user) {
         return "signup";
     }
+
     @RequestMapping(value = "/signup", method = RequestMethod.POST)
-    public String signupPOST(User user, BindingResult result, Model model){
-        if(result.hasErrors()){
+    public String signupPOST(User user, BindingResult result, Model model) {
+        if (result.hasErrors()) {
             return "redirect:/signup";
         }
 
         User exists = userService.findByUsername(user.getUsername());
 
 
-
-        if(exists == null){
+        if (exists == null) {
             userService.save(user);
-        }
-        else error = false;
+        } else error = false;
         return "redirect:/error";
     }
 
     @RequestMapping(value = "/login", method = RequestMethod.GET)
-    public String loginGET(User user){
+    public String loginGET(User user) {
         return "login";
     }
 
     @RequestMapping(value = "/login", method = RequestMethod.POST)
-    public String loginPOST(User user, BindingResult result, Model model, HttpSession session){
-        if(result.hasErrors()){
+    public String loginPOST(User user, BindingResult result, Model model, HttpSession session) {
+        if (result.hasErrors()) {
             return "login";
         }
         User exists = userService.login(user);
-        if(exists != null){
+        if (exists != null) {
             session.setAttribute("LoggedInUser", exists);
             model.addAttribute("LoggedInUser", exists);
             return "redirect:/userNews";
@@ -71,9 +70,9 @@ public class UserController {
     }
 
     @RequestMapping(value = "/loggedin", method = RequestMethod.GET)
-    public String loggedinGET(HttpSession session, Model model){
+    public String loggedinGET(HttpSession session, Model model) {
         User sessionUser = (User) session.getAttribute("LoggedInUser");
-        if(sessionUser != null){
+        if (sessionUser != null) {
             model.addAttribute("LoggedInUser", sessionUser);
             return "loggedInUser";
         }
@@ -97,9 +96,9 @@ public class UserController {
     }
 
     @RequestMapping(value = "/loggedout", method = RequestMethod.GET)
-    public String loggedoutGET(HttpServletRequest request, HttpServletResponse response){
+    public String loggedoutGET(HttpServletRequest request, HttpServletResponse response) {
         Authentication auth = SecurityContextHolder.getContext().getAuthentication();
-        if (auth != null){
+        if (auth != null) {
             new SecurityContextLogoutHandler().logout(request, response, auth);
         }
         return "redirect:/login?logout"; //redirect to login screen
